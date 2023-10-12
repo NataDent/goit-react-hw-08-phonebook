ae672be7142b0f1de082fa1b34071c4198abc986
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-10-12 13:00:00 +0300

    Working with Modal

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,9 +1,12 @@
+import { useDisclosure } from '@chakra-ui/react';
 import { ContactForm } from 'components/ContactForm';
 import { ContactList } from 'components/ContactList';
 import { Filter } from 'components/Filter';
-import { useEffect } from 'react';
+import { ModalWindow } from 'components/Modal';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/contacts/operations';
+
 import {
   selectContacts,
   selectError,
@@ -15,10 +18,15 @@ const Contacts = () => {
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
   const dispatch = useDispatch();
+  const [isModal, setIsModal] = useState(false);
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
+
+  const toggleModal = () => {
+    setIsModal(isModal === !isModal);
+  };
 
   return (
     <>
@@ -27,6 +35,7 @@ const Contacts = () => {
       <Filter />
       {isLoading && !error && <p fontSize={20}>Request in progress...</p>}
       {contacts ? <ContactList /> : <p fontSize={20}>No contacts</p>}
+      {isModal && <ModalWindow onClose={toggleModal} />}
     </>
   );
 };
