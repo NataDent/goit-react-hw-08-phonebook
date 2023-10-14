941ab4b8ab8b687c73c9d615d3d09dd78eb08640
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-10-14 15:43:55 +0300

    Changed styles on Forms. Everything works

--- src/components/ContactForm/ContactFormStyled.jsx
+++ src/components/ContactForm/ContactFormStyled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Form, ErrorMessage } from 'formik';
+import { Form, ErrorMessage, Field } from 'formik';
 
 // export const FormWrapper = styled.div`
 //   display: flex;
@@ -12,34 +12,50 @@ import { Form, ErrorMessage } from 'formik';
 //   /* border: 1px solid darkgrey; */
 // `;
 
-export const FormStyled = styled(Form)``;
-
-export const Label = styled.label`
-  font-size: 20px;
-  display: block;
-  padding: 4px 0;
-  color: ;
+export const FormStyled = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 4px;
 `;
 
 export const FieldWrapper = styled.div`
-  height: 72px;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  padding: 4px;
+`;
+
+export const InputStyled = styled(Field)`
+  height: 48px;
+  border: 1px solid #97266d;
+  border-radius: 4px;
+  border-color: #97266d;
+  color: #97266d;
+`;
+
+export const Label = styled.label`
+  font-size: 16px;
+  display: block;
+  padding: 4px 0;
+  color: #f687b3;
 `;
 
 export const ErrorMessageStyled = styled(ErrorMessage)`
   color: red;
-  font-weight: bold;
 `;
 
 export const AddButton = styled.button`
-  margin-top: 8px;
-  padding: 4px;
+  font-weight: bold;
+  margin: 8px auto;
+  padding: 8px;
   background-color: #fed7e2;
   color: #521b41;
   border-radius: 4px;
   cursor: pointer;
 
   &:hover {
-    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    box-shadow: rgba(151, 38, 109, 0.2) 0px 7px 29px 0px;
     transform: scale(1.05);
     background-color: #97266d;
     color: #fff;
